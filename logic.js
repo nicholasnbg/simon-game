8ce858d74108a2b529ab99a59e52e435c0687848
nbg <nicholasnbg@gmail.com>
2017-08-02 07:51:27 +1000

    more logic work

--- logic.js
+++ logic.js
@@ -8,22 +8,32 @@ $(document).ready(function(){
   var colors = [
     {
       color: 'green',
-      num: 0
+      num: 0,
+      highlight:"#00FF00" ,
+      unhighlight: "#195D00"
     },
     {
       color: 'red',
-      num: 1
+      num: 1,
+      highlight: "#FF0000" ,
+      unhighlight: "#890016"
     },
     {
       color: 'yellow',
-      num: 2
+      num: 2,
+      highlight: "#FFFF00" ,
+      unhighlight:"#E1C800"
     },
     {
       color: 'blue',
-      num: 3
+      num: 3,
+      highlight: "#0000FF" ,
+      unhighlight:"#000283"
     }
   ];
 
+  //run functions
+  fillCompTurns();
 
 
   //Delcare functions
@@ -33,7 +43,7 @@ $(document).ready(function(){
 
   function fillCompTurns(){
     for(i=0;i<20;i++){
-      
+      computerTurns.push(pickRandom());
     }
   };
 
