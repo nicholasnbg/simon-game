5d29088ab5fc43d097dfd559b3761cc6d4130de3
nbg <nicholasnbg@gmail.com>
2017-08-01 19:39:01 +1000

    logic changes

---
+++ logic.js
@@ -0,0 +1,42 @@
+$(document).ready(function(){
+
+  //Define global variables
+  var start = false;
+  var turn;
+  var computerTurns = [];
+
+  var colors = [
+    {
+      color: 'green',
+      num: 0
+    },
+    {
+      color: 'red',
+      num: 1
+    },
+    {
+      color: 'yellow',
+      num: 2
+    },
+    {
+      color: 'blue',
+      num: 3
+    }
+  ];
+
+
+
+  //Delcare functions
+  function pickRandom(){
+    return Math.floor(Math.random()*4);
+  }
+
+  function fillCompTurns(){
+    for(i=0;i<20;i++){
+      
+    }
+  };
+
+
+
+});
